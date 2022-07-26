467eb88d0a03cf278bb68be98f9db4ca55305e95
dcoo <djc4ku@virginia.edu>
2022-07-26 17:22:36 -0400

    main: parse urls

--- forms_service.py
+++ forms_service.py
@@ -1,24 +1,24 @@
-from __future__ import print_function
-
 import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 
-def get_values(spreadsheet_id, range_name):
+def get_urls(spreadsheet_id, range_name):
     creds, _ = google.auth.default()
+
     try:
         service = build('sheets', 'v4', credentials=creds)
 
-        result = service.spreadsheets().values().get(
-            spreadsheetId=spreadsheet_id, range=range_name).execute()
-        rows = result.get('values', [])
-        print(f"{len(rows)} rows retrieved")
+        result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+        urls = result.get('values', [])
+        urls.pop(0)
+        urls = [ url[0] for url in urls ]
+        print(urls)
         return result
     except HttpError as error:
         print(f"An error occurred: {error}")
         return error
 
-
 if __name__ == '__main__':
-    get_values("1oizPnNYIEzSLL6CrjlAMZdySw90jfTJf_2X9SFHejTM", "A1:C2")
+    get_urls("1oizPnNYIEzSLL6CrjlAMZdySw90jfTJf_2X9SFHejTM", "'Form Responses 1'!B:B")
+
